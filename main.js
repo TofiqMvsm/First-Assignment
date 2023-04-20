a681522f6a326fc0ceb77c859074d7f307bd8693
WIN-6LUN2D0CH7K\user <tofiqmvsm2003@gmail.com>
2023-04-20 20:38:26 +0400

    first assignment

---
+++ main.js
@@ -0,0 +1,13 @@
+function ourServices(e, ourServicesName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(ourServicesName).style.display = "flex";
+    e.currentTarget.className += " active";
+  }
